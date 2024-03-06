2bc3401b5fa5229c9d070695387ddd8a56671051
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-06 21:21:49 +0100

    changement vers port4000 + erreur CORS + affichage 1er livre sur HP

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("teeest");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+app.use("/api/books", (req, res, next) => {
+  const books = [
+    {
+      _id: "oeihfzeoi",
+      title: "Titre du livre",
+      author: "Auteur du livre",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      year: 2023,
+      genre: "Genre du livre",
+      ratings: [
+        {
+          userId: "identifiant MongoDB unique de l'utilisateur",
+          grade: 5,
+        },
+      ],
+      averageRating: 4,
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
