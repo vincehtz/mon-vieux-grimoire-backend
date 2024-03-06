6af2549853a67d9f0b3fcd9c4283b64a7694231e
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-06 22:44:20 +0100

    ajout des routes Put et Delete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const Book = require("./models/Book");
+
 mongoose
   .connect(
     "mongodb+srv://vince_htz:lMzkVi5SEOIH5pVo@clustermvg.aypd75t.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMVG"
@@ -26,30 +28,38 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/books", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: "objet créé" });
+  delete req.body._id;
+  const book = new Book({
+    ...req.body,
+  });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
 });
 
 app.get("/api/books", (req, res, next) => {
-  const books = [
-    {
-      userId: "identifiant MongoDB unique de l'utilisateur",
-      title: "Titre du livre",
-      author: "Auteur du livre",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      year: 2023,
-      genre: "Genre du livre",
-      ratings: [
-        {
-          userId: "identifiant MongoDB unique de l'utilisateur",
-          grade: 5,
-        },
-      ],
-      averageRating: 4,
-    },
-  ];
-  res.status(200).json(books);
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
