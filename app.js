c4103b8bc517a284eefc30e61b17ac680b9e023e
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-06 20:52:52 +0100

    MAJ server.js (ajout du code stabiliser et améliorer)

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("teeest");
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: "Votre requête a bien été reçue !" });
 });
 
