8cab11b711fdd415bdd4d8b1c9ef6b07527ef713
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-14 12:47:15 +0100

    Sharp configuré (naming des fichiers MAJ dans le dossier) + ajouté sur les routes apres le multer

---
+++ middleware/sharp.js
@@ -0,0 +1,28 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
+module.exports = (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const imagePath = req.file.path;
+  const lastDot = imagePath.lastIndexOf(".");
+  const extension = imagePath.substring(lastDot);
+  const imageName = imagePath.substring(0, lastDot);
+
+  sharp(imagePath)
+    .resize({ width: 800 })
+    .toFormat("webp")
+    .toFile(imageName + "_resized" + extension, (err, info) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: "Erreur lors du traitement de l'image." });
+      }
+      fs.unlinkSync(imagePath);
+      req.file.path = imageName + "_resized" + extension; // Mise à jour du chemin de l'image dans la requête
+      next();
+    });
+};
